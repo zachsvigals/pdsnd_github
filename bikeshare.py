0e8b9a4c291873fb2d1b92e6f87d7bf4da79d425
Zach Svigals <zach@curai.com>
2019-02-27 23:37:06 -0800

    abreviated statistics

--- bikeshare.py
+++ bikeshare.py
@@ -77,7 +77,7 @@ def load_data(city, month, day):
     return df
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays stats on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -98,7 +98,7 @@ def time_stats(df):
     print('-'*40)
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays stats on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -120,7 +120,7 @@ def station_stats(df):
     print('-'*40)
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays stats on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -138,7 +138,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays stats on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
